1bc4e28430d8159247130faa3bd6614d609ffc7e
NoyCoca <noycoca123@gmail.com>
2021-08-09 16:55:29 +0300

    heroku changes

--- server/server.js
+++ server/server.js
@@ -3,16 +3,27 @@ env.config();
 const connection = require('./DB')
 const express = require('express');
 const app = express();
-const studentRouter= require('./router/studentRouter')
+const studentRouter = require('./router/studentRouter')
 const cors = require('cors');
+const path = require('path');
+const PORT = process.env.PORT || 8080
+
 app.use(express.json())
 
 connection.on('error', () => {
     console.log("error")
 })
 app.use(cors())
-app.listen(8080, () => {
-    console.log("confected to server")
+app.listen(PORT, () => {
+    console.log("confected to server ")
 })
 
 app.use('/students', studentRouter)
+// ***********************************************
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
+    })
+}
+// ***********************************************
