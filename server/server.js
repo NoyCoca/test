277d025fe36ae18d910525143da9c1fe3afdb83c
NoyCoca <noycoca123@gmail.com>
2021-08-09 17:18:52 +0300

    add

--- server/server.js
+++ server/server.js
@@ -21,9 +21,10 @@ app.listen(PORT, () => {
 app.use('/students', studentRouter)
 // ***********************************************
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')))
+    app.use(express.static(path.join(__dirname, '../client/build')));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
-    })
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    });
 }
+
 // ***********************************************
